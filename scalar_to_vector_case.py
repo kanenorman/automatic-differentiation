47a95c4286cdb290cf2e33df6ff7dc0374d37313
Kane Norman <kanenorman@fas.harvard.edu>
2022-12-10 11:37:33 -0500

    Updated Example of Multivariable Function (#58)

--- scalar_to_vector_case.py
+++ scalar_to_vector_case.py
@@ -4,23 +4,23 @@ import autodiff_team29.elementaries as E
 from autodiff_team29 import VectorFunction
 import numpy as np
 
-# 'Scalar to Vector' case
-x = Node("x", value=2, derivative=1)  
-
+# Vector Case
+x1 = Node("x1", np.pi / 2, 1, seed_vector=[0, 1])
+x2 = Node("x2", np.pi / 16, 1, seed_vector = [0,1])
+x3 = Node("x3", np.e, 1, seed_vector = [0,1])
 
 print('Node aspects:')
-print(f"Symbol: {x.symbol}")
-print(f'Value: {x.value}')
-print(f"Derivative: {x.derivative}")
+print(f"Symbols: x1: {x1.symbol}, x2: {x2.symbol}, x3: {x3.symbol}")
+print(f'Values: x1: {x1.value}, x2: {x2.value}, x3: {x3.value}')
+print(f"Derivatives: x1: {x1.derivative}, x2: {x2.derivative}, x3: {x3.derivative}")
 
-# Defining multiple functions: sin(x), cos(x) and exp(x)
-f1, f2, f3 = E.sin(x), E.cos(x), E.exp(x)
+# Defining a function `f`
+f1 = x1 + x2 * x3
 
 # Instantiating the VectorFunction
-f = VectorFunction([f1, f2, f3])
+f = VectorFunction([f1])
 
 # Inspect f --> new `Node` with consistent representation
-print("New Node representation:")
 print("Symbol:", f.symbol)
 print("Value", f.value)
 print("Jacobian", f.jacobian)
